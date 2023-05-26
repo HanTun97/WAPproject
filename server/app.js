55201d29d50f03b679d052658d79a1d7b7c6fa2f
Han Tun <45828855+HanTun97@users.noreply.github.com>
2023-05-25 22:32:13 -0500

    final project

--- server/app.js
+++ server/app.js
@@ -25,6 +25,10 @@ app.use((req, res, next) => {
 
 app.use('/shopping', productRouter);
 
+app.use((req, res, next)=>{
+    res.status(404).json({error: 'File Not Found'});
+})
+
 app.use((error, req, res, next)=>{
     res.status(500).json({error: 'Invalid username and password!'});
 })
